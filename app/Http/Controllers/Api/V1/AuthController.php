f9f2eddae90412c57691bfd3081dde87ec7378d3
Edvaldo Torres <57301962+edvaldotorres@users.noreply.github.com>
2024-02-01 23:10:17 -0300

    feature: add JWTToken middleware

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -18,29 +18,8 @@ class AuthController extends Controller
         return $this->authService->authenticate($credentials);
     }
 
-    // public function me()
-    // {
-    //     return response()->json(auth()->user());
-    // }
-
-    // public function logout()
-    // {
-    //     auth()->logout();
-
-    //     return response()->json(['message' => 'Successfully logged out']);
-    // }
-
-    // public function refresh()
-    // {
-    //     return $this->respondWithToken(auth()->refresh());
-    // }
-
-    // protected function respondWithToken($token)
-    // {
-    //     return response()->json([
-    //         'access_token' => $token,
-    //         'token_type' => 'bearer',
-    //         'expires_in' => auth()->factory()->getTTL() * 60
-    //     ]);
-    // }
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::prefix('v1')->group(function () {
         Route::post('login', [AuthController::class, 'login']);
         Route::post('register', [AuthController::class, 'register']);
     });
-    
-    Route::apiResource('tasks', TaskController::class);
+    Route::middleware('jwt.api')->group(function () {
+        Route::apiResource('tasks', TaskController::class);
+        Route::get('me', [AuthController::class, 'me']);
+    });
 });
