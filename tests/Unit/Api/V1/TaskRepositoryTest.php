38806fa7a4521a59327cebd2477a93ac59acd08a
Edvaldo Torres <57301962+edvaldotorres@users.noreply.github.com>
2024-02-01 23:04:00 -0300

    refactor: add tipagem in the code

--- tests/Unit/Api/V1/TaskRepositoryTest.php
+++ tests/Unit/Api/V1/TaskRepositoryTest.php
@@ -24,7 +24,7 @@ class TaskRepositoryTest extends TestCase
         $this->taskRepository = new TaskRepository();
     }
 
-    public function test_all_tasks_for_user()
+    public function test_all_tasks_for_user(): void
     {
         // Test to ensure all tasks for a user are correctly retrieved
         $tasks = Task::factory()->count(5)->create(['user_id' => $this->user->id]);
@@ -32,7 +32,7 @@ class TaskRepositoryTest extends TestCase
         $this->assertEquals($tasks->count(), $allTasks->count(), "The count of tasks does not match.");
     }
 
-    public function test_creates_a_task()
+    public function test_creates_a_task(): void
     {
         // Test to ensure a task is correctly created
         $taskData = Task::factory()->make()->toArray();
@@ -40,7 +40,7 @@ class TaskRepositoryTest extends TestCase
         $this->assertModelData($taskData, $createdTask->toArray());
     }
 
-    public function test_finds_a_task()
+    public function test_finds_a_task(): void
     {
         // Test to ensure a specific task can be found by ID
         $task = Task::factory()->create(['user_id' => $this->user->id]);
@@ -48,7 +48,7 @@ class TaskRepositoryTest extends TestCase
         $this->assertModelData($task->toArray(), $foundTask->toArray());
     }
 
-    public function test_updates_a_task()
+    public function test_updates_a_task(): void
     {
         // Test to ensure a task can be updated
         $task = Task::factory()->create(['user_id' => $this->user->id]);
@@ -63,7 +63,7 @@ class TaskRepositoryTest extends TestCase
         $this->assertModelData($taskData, $updatedTask->toArray());
     }
 
-    public function test_deletes_a_task()
+    public function test_deletes_a_task(): void
     {
         // Test to ensure a task can be deleted
         $task = Task::factory()->create(['user_id' => $this->user->id]);
@@ -71,7 +71,7 @@ class TaskRepositoryTest extends TestCase
         $this->assertNull(Task::find($task->id), "The task should be deleted but was found.");
     }
 
-    protected function assertModelData(array $expected, array $actual)
+    protected function assertModelData(array $expected, array $actual): void
     {
         foreach ($expected as $key => $value) {
             $this->assertEquals($value, $actual[$key], "The {$key} of the task does not match.");
